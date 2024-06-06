f81e400cc9f378494295c999c55d715888cf2c0c
Michael Bardwell <mike.d.bardwell@pm.me>
2024-06-05 21:57:14 -0400

    Rename vars to use PCB names

--- led-hoodie/led-hoodie.ino
+++ led-hoodie/led-hoodie.ino
@@ -8,9 +8,9 @@
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN1        0 // On Trinket or Gemma, suggest changing this to 1
-#define PIN2        1 // On Trinket or Gemma, suggest changing this to 1
-#define PIN3        2 // On Trinket or Gemma, suggest changing this to 1
+#define PIN_LED_ARM_L        0 // On Trinket or Gemma, suggest changing this to 1
+#define PIN_LED_ARM_R        1 // On Trinket or Gemma, suggest changing this to 1
+#define PIN_LED_BCK_1        2 // On Trinket or Gemma, suggest changing this to 1
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 16 // Popular NeoPixel ring size
@@ -19,9 +19,9 @@
 // and which pin to use to send signals. Note that for older NeoPixel
 // strips you might need to change the third parameter -- see the
 // strandtest example for more information on possible values.
-Adafruit_NeoPixel pixels1(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixels2(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixels3(NUMPIXELS, PIN3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel led_arm_l(NUMPIXELS, PIN_LED_ARM_L, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel led_arm_r(NUMPIXELS, PIN_LED_ARM_R, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel led_bck_1(NUMPIXELS, PIN_LED_BCK_1, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
 
@@ -33,18 +33,18 @@ void setup() {
 #endif
   // END of Trinket-specific code.
 
-  pixels1.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels2.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels3.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels1.setBrightness(10);
-  pixels2.setBrightness(10);
-  pixels3.setBrightness(10);
+  led_arm_l.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  led_arm_r.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  led_bck_1.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  led_arm_l.setBrightness(10);
+  led_arm_r.setBrightness(10);
+  led_bck_1.setBrightness(10);
 }
 
 void loop() {
-  pixels1.clear(); // Set all pixel colors to 'off'
-  pixels2.clear(); // Set all pixel colors to 'off'
-  pixels3.clear(); // Set all pixel colors to 'off'
+  led_arm_l.clear(); // Set all pixel colors to 'off'
+  led_arm_r.clear(); // Set all pixel colors to 'off'
+  led_bck_1.clear(); // Set all pixel colors to 'off'
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
@@ -52,13 +52,13 @@ void loop() {
 
     // pixels1.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
-    pixels1.setPixelColor(i, pixels1.Color(0, 150, 0));
-    pixels2.setPixelColor(i, pixels2.Color(0, 150, 0));
-    pixels3.setPixelColor(i, pixels3.Color(0, 150, 0));
+    led_arm_l.setPixelColor(i, led_arm_l.Color(0, 150, 0));
+    led_arm_r.setPixelColor(i, led_arm_r.Color(0, 150, 0));
+    led_bck_1.setPixelColor(i, led_bck_1.Color(0, 150, 0));
 
-    pixels1.show();   // Send the updated pixel colors to the hardware.
-    pixels2.show();   // Send the updated pixel colors to the hardware.
-    pixels3.show();   // Send the updated pixel colors to the hardware.
+    led_arm_l.show();   // Send the updated pixel colors to the hardware.
+    led_arm_r.show();   // Send the updated pixel colors to the hardware.
+    led_bck_1.show();   // Send the updated pixel colors to the hardware.
 
     delay(DELAYVAL); // Pause before next pass through loop
   }
