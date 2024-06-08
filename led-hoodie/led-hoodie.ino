9fce44ca3d1e16ad699cd8b137a008d1ab819c02
Michael Bardwell <mike.d.bardwell@pm.me>
2024-06-08 17:43:26 -0400

    Swap Adafruit_NeoPixel libs for FastLED

--- led-hoodie/led-hoodie.ino
+++ led-hoodie/led-hoodie.ino
@@ -8,54 +8,44 @@
  * Adafruit NeoPixel library
 */
 
-#include <Adafruit_NeoPixel.h>
-#include <Adafruit_NeoMatrix.h>
+#include <FastLED.h>
+#include <FastLED_NeoMatrix.h>
 
 constexpr int16_t GPIO_LED_ARM_L = 1;  // PIN 1 on v0.1.0 schematics
 constexpr int16_t GPIO_LED_ARM_R = 4;  // PIN 2 on v0.1.0 schematics
 constexpr int16_t GPIO_LED_BCK_1 = 2;  // PIN 4 on v0.1.0 schematics
 constexpr int16_t GPIO_LED_BCK_2 = 5;  // PIN 5 on v0.1.0 schematics
-constexpr int16_t GPIO_LED_EAR_1 = 0;  // PIN 6 on v0.1.0 schematics
-constexpr int16_t GPIO_LED_EAR_2 = 3;  // PIN 7 on v0.1.0 schematics
+constexpr int16_t GPIO_LED_EAR_L = 0;  // PIN 6 on v0.1.0 schematics
+constexpr int16_t GPIO_LED_EAR_R = 3;  // PIN 7 on v0.1.0 schematics
 constexpr int16_t N_ARM_PIXELS = 60;
 constexpr int16_t N_EAR_PIXELS = 16;
 constexpr int16_t N_BCK_MATRIX_HGT = 8;
 constexpr int16_t N_BCK_MATRIX_WDT = 32;
 
-Adafruit_NeoPixel led_arm_l(N_ARM_PIXELS, GPIO_LED_ARM_L);
-Adafruit_NeoPixel led_arm_r(N_ARM_PIXELS, GPIO_LED_ARM_R);
-Adafruit_NeoMatrix led_bck_1(N_BCK_MATRIX_WDT, N_BCK_MATRIX_HGT, GPIO_LED_BCK_1, NEO_MATRIX_TOP + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS);
-Adafruit_NeoMatrix led_bck_2(N_BCK_MATRIX_WDT, N_BCK_MATRIX_HGT, GPIO_LED_BCK_2, NEO_MATRIX_TOP + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS);
-Adafruit_NeoPixel led_ear_1(N_EAR_PIXELS, GPIO_LED_EAR_1);
-Adafruit_NeoPixel led_ear_2(N_EAR_PIXELS, GPIO_LED_EAR_2);
+CRGB led_arm_l_buf[N_ARM_PIXELS];
+CRGB led_arm_r_buf[N_ARM_PIXELS];
+CRGB led_bck_1_buf[N_BCK_MATRIX_WDT* N_BCK_MATRIX_HGT];
+CRGB led_bck_2_buf[N_BCK_MATRIX_WDT* N_BCK_MATRIX_HGT];
+CRGB led_ear_l_buf[N_EAR_PIXELS];
+CRGB led_ear_r_buf[N_EAR_PIXELS];
+
+FastLED_NeoMatrix led_bck_1(led_bck_1_buf, N_BCK_MATRIX_WDT, N_BCK_MATRIX_HGT, GPIO_LED_BCK_1, NEO_MATRIX_TOP + NEO_MATRIX_LEFT);
+FastLED_NeoMatrix led_bck_2(led_bck_2_buf, N_BCK_MATRIX_WDT, N_BCK_MATRIX_HGT, GPIO_LED_BCK_2, NEO_MATRIX_TOP + NEO_MATRIX_LEFT);
 
 constexpr int RETURN_SUCCESS = 0;
 int thread_onboard_led_handler();
+int setup_arm();
 int thread_arm_handler();
+int setup_ear();
 int thread_ear_handler();
+int setup_bck();
 int thread_bck_handler();
 
 void setup() {
-  Serial.println("LED Hoodie");
-
   pinMode(LED_BUILTIN, OUTPUT);
-  led_arm_l.begin();
-  led_arm_r.begin();
-  led_bck_1.begin();
-  led_bck_1.setTextWrap(false);
-  led_bck_1.setTextColor(led_bck_1.Color(0, 0, 150));
-  led_bck_2.begin();
-  led_bck_2.setTextWrap(false);
-  led_bck_2.setTextColor(led_bck_2.Color(150, 150, 0));
-  led_ear_1.begin();
-  led_ear_2.begin();
-
-  led_arm_l.setBrightness(10);
-  led_arm_r.setBrightness(10);
-  led_bck_1.setBrightness(10);
-  led_bck_2.setBrightness(10);
-  led_ear_1.setBrightness(10);
-  led_ear_2.setBrightness(10);
+  setup_arm();
+  setup_ear();
+  setup_bck();
 }
 
 void loop() {
@@ -63,6 +53,8 @@ void loop() {
   thread_arm_handler();
   thread_ear_handler();
   thread_bck_handler();
+
+  FastLED.show();
 }
 
 int thread_onboard_led_handler() {
@@ -77,46 +69,87 @@ int thread_onboard_led_handler() {
   return RETURN_SUCCESS;
 }
 
+int setup_arm() {
+  FastLED.addLeds<NEOPIXEL, GPIO_LED_ARM_L>(led_arm_l_buf, N_ARM_PIXELS);
+  FastLED.addLeds<NEOPIXEL, GPIO_LED_ARM_R>(led_arm_r_buf, N_ARM_PIXELS);
+
+  // led_arm_l.setBrightness(10);
+  // led_arm_r.setBrightness(10);
+
+  return RETURN_SUCCESS;
+}
+
 int thread_arm_handler() {
-  static uint32_t last_time = 250;
-  if (millis() - last_time < 1000) {
+  static uint32_t last_time = 0;
+  static uint32_t i = 0;
+  if (millis() - last_time < 100) {
     return RETURN_SUCCESS;
   }
 
-  led_arm_l.clear();
-  led_arm_r.clear();
-  for(int i=0; i<N_ARM_PIXELS; i++) {
-    led_arm_l.setPixelColor(i, led_arm_l.Color(0, 150, 0));
-    led_arm_r.setPixelColor(i, led_arm_r.Color(150, 0, 0));
-
-    led_arm_l.show();
-    led_arm_r.show();
-    delay(50);
+  led_arm_l_buf[i] = CRGB::Red;
+  led_arm_r_buf[i] = CRGB::Green;
+  if (i > N_ARM_PIXELS) {
+    for (int j = 0; j < N_ARM_PIXELS; j++) {
+      led_arm_l_buf[j] = CRGB::Black;
+      led_arm_r_buf[j] = CRGB::Black;
+    }
+    i = 0;
   }
+  else
+   i += 1;
 
   last_time = millis();
   return RETURN_SUCCESS;
 }
 
+int setup_ear() {
+  FastLED.addLeds<NEOPIXEL, GPIO_LED_EAR_L>(led_ear_l_buf, N_EAR_PIXELS);
+  FastLED.addLeds<NEOPIXEL, GPIO_LED_EAR_R>(led_ear_r_buf, N_EAR_PIXELS);
+
+  // led_ear_1.setBrightness(10);
+  // led_ear_2.setBrightness(10);
+
+  return RETURN_SUCCESS;
+}
+
 int thread_ear_handler() {
-  static uint32_t last_time = 750;
-  if (millis() - last_time < 1000) {
+  static uint32_t last_time = 0;
+  static uint32_t i = 0;
+  if (millis() - last_time < 100) {
     return RETURN_SUCCESS;
   }
 
-  led_ear_1.clear();
-  led_ear_2.clear();
-
-  for(int i=0; i<N_EAR_PIXELS; i++) {
-    led_ear_1.setPixelColor(i, led_ear_1.Color(150, 0, 150));
-    led_ear_2.setPixelColor(i, led_ear_2.Color(0, 150, 150));
-
-    led_ear_1.show();
-    led_ear_2.show();
-    delay(50);
+  led_ear_l_buf[i] = CRGB::Blue;
+  led_ear_r_buf[i] = CRGB::Red;
+  if (i > N_EAR_PIXELS) {
+    for (int j = 0; j < N_EAR_PIXELS; j++) {
+      led_ear_l_buf[j] = CRGB::Black;
+      led_ear_r_buf[j] = CRGB::Black;
+    }
+    i = 0;
   }
+  else
+   i += 1;
 
   last_time = millis();
+  return RETURN_SUCCESS;
+}
+
+int setup_bck() {
+  FastLED.addLeds<NEOPIXEL, GPIO_LED_BCK_1>(led_bck_1_buf, N_BCK_MATRIX_WDT* N_BCK_MATRIX_HGT);
+  FastLED.addLeds<NEOPIXEL, GPIO_LED_BCK_2>(led_bck_2_buf, N_BCK_MATRIX_WDT* N_BCK_MATRIX_HGT);
+
+  led_bck_1.begin();
+  led_bck_1.setTextWrap(false);
+  led_bck_1.setTextColor(led_bck_1.Color(0, 0, 150));
+
+  led_bck_2.begin();
+  led_bck_2.setTextWrap(false);
+  led_bck_2.setTextColor(led_bck_2.Color(0, 150, 0));
+
+  led_bck_1.setBrightness(10);
+  led_bck_2.setBrightness(10);
+
   return RETURN_SUCCESS;
 }
 
